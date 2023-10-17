bac6ad0cf31b892efdabb30d63afe848a015d0d1
KshitijKarnawat <kshitijkarnawat009@gmail.com>
2023-10-17 16:13:37 -0400

    Add camera and detector hpp files

---
+++ include/detector.hpp
@@ -0,0 +1,38 @@
+/**
+ * @file detector.hpp
+ * @author Kshitij Karnawat (@KshitijKarnawat)
+ * @author Hritvik Choudhary (hac@umd.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2023-10-17
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#pragma once
+
+#include <opencv2/opencv.hpp>
+
+namespace acme {
+
+  /**
+   * @brief 
+   * 
+   */
+	class Detector{
+		/**
+		 * @brief Construct a new Detector object
+		 * 
+		 */
+		Detector();
+
+		/**
+		 * @brief Destroy the Detector object
+		 * 
+		 */
+		~Detector();
+		
+		
+  };
+}
