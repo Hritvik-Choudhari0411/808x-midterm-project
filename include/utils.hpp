7dabb728d0150b5d0c3a1535f7b06cb3d022bd2a
KshitijKarnawat <kshitijkarnawat009@gmail.com>
2023-10-24 01:07:45 -0400

    Task 1.6.3: Initalize Utils Header File Stub

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,10 +1,56 @@
 /**
  * @file utils.hpp
- * @author your name (you@domain.com)
- * @brief 
+ * @author Kshitij Karnawat (@KshitijKarnawat)
+ * @author Hritvik Choudhary (hac@umd.edu)
+ * @brief Utility functions header file.
  * @version 0.1
  * @date 2023-10-23
  * 
  * @copyright Copyright (c) 2023
  * 
  */
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+
+namespace acme {
+    class Utils{
+        public:
+       
+       /**
+        * @brief Resize the image.
+        * 
+        */
+         void ResizeImage();
+
+        /**
+         * @brief Transform pose to world coordinates.
+         * 
+         */
+            void TransformPose();
+
+        /**
+         * @brief Calculate the IoU.
+         * 
+         */
+        void CalculateIoU();
+
+        /**
+         * @brief Get the bounding boxes for a human.
+         * 
+         */
+        void GetBoundingBox();
+
+        /**
+         * @brief Draw the bounding boxes on the frame.
+         * 
+         */
+        void DrawBoundingBox();
+
+
+    };
+}
