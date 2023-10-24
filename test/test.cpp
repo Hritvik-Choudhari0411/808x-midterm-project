1a0133704bc7f3bffa7bc04b0e67b958d8805ed1
Hritvik Choudhari <hritvikchoudhari@gmail.com>
2023-10-24 18:24:28 -0400

    Update author information

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,7 @@
 /**
  * @file test.cpp
  * @author Kshitij Karnawat (@KshitijKarnawat)
- * @author Hritvik Choudhary (hac@umd.edu)
+ * @author Hritvik Choudhari (hac@umd.edu)
  * @brief Test file for the project.
  * @version 0.1
  * @date 2023-10-24
@@ -13,3 +13,9 @@
 #include <gtest/gtest.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
+
+#include <utils.hpp>
+
+TEST(CalculateIoU, this_should_pass) {
+  EXPECT_EQ(1, 1);
+}
