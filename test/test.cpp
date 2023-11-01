fceacd7efa0ef264d50529d6f9c736d5a48963a4
KshitijKarnawat <kshitijkarnawat009@gmail.com>
2023-11-01 07:08:18 -0400

    Tests modified to fit trackerLib

--- test/test.cpp
+++ test/test.cpp
@@ -12,7 +12,18 @@
 
 #include <gtest/gtest.h>
 
+#include <opencv2/dnn/dnn.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+#include <string>
 
-TEST(dummy_test, this_should_pass) { EXPECT_EQ(1, 1); }
+#include "../include/tracker.hpp"
+
+TEST(test, test) {
+  cv::Mat image = cv::imread("model/test.jpg");
+  acme::Tracker tracker;
+  std::string label = "person";
+  int top = 10;
+  int left = 10;
+  ASSERT_NO_THROW(tracker.DisplayLabels(image, label, top, left));
+}
