0ab3417b401bd1cee83bd85e72cb3295be2dc540
KshitijKarnawat <kshitijkarnawat009@gmail.com>
2023-11-01 07:06:37 -0400

    Tracker library

--- include/tracker.hpp
+++ include/tracker.hpp
@@ -21,6 +21,10 @@
 #include <opencv2/opencv.hpp>
 
 namespace acme{
+/**
+ * @brief Class for tracking humans in a video.
+ * 
+ */
 class Tracker{
  private:
   const float SCORE_THRESHOLD = 0.5;
@@ -29,13 +33,36 @@ class Tracker{
 
  public:
 
+  /**
+   * @brief Display the labels on the frame.
+   * 
+   * @param frame 
+   * @param label 
+   * @param top 
+   * @param left 
+   */
   void DisplayLabels(const cv::Mat &frame, std::string label,
                   int top, int left);
 
+  /**
+   * @brief Draw the bounding boxes on the frame.
+   * 
+   * @param frame 
+   * @param detections 
+   * @param person 
+   * @return cv::Mat 
+   */
   cv::Mat DrawBoxes(cv::Mat frame,
                        std::vector<cv::Mat> detections,
                        std::vector<std::string> person);
 
+  /**
+   * @brief Detect the humans in the frame.
+   * 
+   * @param frame 
+   * @param model 
+   * @return std::vector<cv::Mat> 
+   */
   std::vector<cv::Mat> DetectNN(cv::Mat frame, cv::dnn::Net model);
 };
 }
