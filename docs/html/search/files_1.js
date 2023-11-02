b2aa472f2dd8bcee8146bfb4e61644f3f9a6089c
KshitijKarnawat <kshitijkarnawat009@gmail.com>
2023-11-01 21:58:32 -0400

    Update Docs

---
+++ docs/html/search/files_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['tracker_2ecpp_15',['tracker.cpp',['../tracker_8cpp.html',1,'']]],
+  ['tracker_2ehpp_16',['tracker.hpp',['../tracker_8hpp.html',1,'']]]
+];
---
+++ docs/html/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['nms_5fthreshold_22',['NMS_THRESHOLD',['../classacme_1_1Tracker.html#a2ed0ea6b533b08d214ef6c8c41286765',1,'acme::Tracker']]]
+];
